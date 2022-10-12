7b1b37e3741734d9c2871029cfc6515a434f2446
Brian Nguyen <80137172+SaintMcMuffins@users.noreply.github.com>
2022-10-11 20:00:27 -0700

    Initial Commit

---
+++ J.I.M/J_I_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  J_I_MApp.swift
+//  J.I.M
+//
+//  Created by Brian Nguyen on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct J_I_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
