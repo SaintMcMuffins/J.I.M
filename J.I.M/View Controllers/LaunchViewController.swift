aca8355c1bb079da841977dee8f793196fd4a0a7
jmanaois <100259401+jmanaois@users.noreply.github.com>
2022-10-26 11:01:13 -0700

    reset register, add launch view controller

---
+++ J.I.M/View Controllers/LaunchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchViewController.swift
+//  J.I.M
+//
+//  Created by Julian Manaois on 10/26/22.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    @IBOutlet weak var logo: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
