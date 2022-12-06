ccac50e5a2a10723f81b76ac009e5ce66469e21d
jmanaois <100259401+jmanaois@users.noreply.github.com>
2022-12-06 10:14:59 -0800

    edit login

---
+++ J.I.M/Login1View.swift
@@ -0,0 +1,68 @@
+//
+//  login.swift
+//  J.I.M
+//
+//  Created by Julian Manaois on 10/18/22.
+//
+
+import UIKit
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+class Login1View: UIViewController {
+
+	@IBOutlet var labelTitle: UILabel!
+	@IBOutlet var labelSubTitle: UILabel!
+	@IBOutlet var imageViewLogo: UIImageView!
+	@IBOutlet var textFieldEmail: UITextField!
+	@IBOutlet var textFieldPassword: UITextField!
+	@IBOutlet var buttonHideShowPassword: UIButton!
+
+	//-------------------------------------------------------------------------------------------------------------------------------------------
+	override func viewDidLoad() {
+
+		super.viewDidLoad()
+
+		textFieldEmail.setLeftPadding(value: 15)
+		textFieldPassword.setLeftPadding(value: 15)
+		textFieldPassword.setRightPadding(value: 40)
+
+		loadData()
+	}
+
+	// MARK: - Data methods
+	//-------------------------------------------------------------------------------------------------------------------------------------------
+	func loadData() {
+
+		labelTitle.text = "Welcome to\nAppDesignKit"
+		labelSubTitle.text = "An exciting place for the whole family to shop."
+	}
+
+	// MARK: - User actions
+	//-------------------------------------------------------------------------------------------------------------------------------------------
+	@IBAction func actionHideShowPassword(_ sender: Any) {
+
+		buttonHideShowPassword.isSelected = !buttonHideShowPassword.isSelected
+		textFieldPassword.isSecureTextEntry = !buttonHideShowPassword.isSelected
+	}
+
+	//-------------------------------------------------------------------------------------------------------------------------------------------
+	@IBAction func actionLogin(_ sender: Any) {
+
+		print(#function)
+		dismiss(animated: true)
+	}
+
+	//-------------------------------------------------------------------------------------------------------------------------------------------
+	@IBAction func actionForgotPassword(_ sender: Any) {
+
+		print(#function)
+		dismiss(animated: true)
+	}
+
+	//-------------------------------------------------------------------------------------------------------------------------------------------
+	@IBAction func actionSignUp(_ sender: Any) {
+
+		print(#function)
+		dismiss(animated: true)
+	}
+}
